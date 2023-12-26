fa2b8f5ecef50c760cf252bc41ca00b79594fb5e
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2023-12-26 23:59:53 +0700

    Set tup themes

--- PokemonApp/app/src/main/java/com/nguyenhoangthanhan/pokemonapp/ui/theme/Color.kt
+++ PokemonApp/app/src/main/java/com/nguyenhoangthanhan/pokemonapp/ui/theme/Color.kt
@@ -10,6 +10,9 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val LightBlue = Color(0xFFBAC7FF)
+val lightGrey = Color(0xFFAAAAAA)
+
 val HPColor = Color(0xFFF5FF00)
 val AtkColor = Color(1f, 0f, 0f, 0.66f)
 val DefColor = Color(0f, 0f, 1f, 0.44f)
