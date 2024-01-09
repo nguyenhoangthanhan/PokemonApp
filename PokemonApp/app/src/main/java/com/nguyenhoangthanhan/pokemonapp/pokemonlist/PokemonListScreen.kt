f5e07c0a893af9e63e8d48989f7a312d85e2a628
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2024-01-09 12:12:44 +0700

    Implemented Pokémon Detail Screen part 3. Completed Pokemon Application

--- PokemonApp/app/src/main/java/com/nguyenhoangthanhan/pokemonapp/pokemonlist/PokemonListScreen.kt
+++ PokemonApp/app/src/main/java/com/nguyenhoangthanhan/pokemonapp/pokemonlist/PokemonListScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -169,7 +170,12 @@ fun PokemonList(
         }
         items(itemCount) {
             if (it >= itemCount - 1 && !endReached && !isLoading && !isSearching) {
-                viewModel.loadPokemonPaginated()
+                LaunchedEffect(
+                    key1 = true,
+                    block = {
+                        viewModel.loadPokemonPaginated()
+                    }
+                )
             }
             PokedexEntry(
                 entry = pokemonList[it],
